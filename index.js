7dbaa5ad9bc27785e99c77cc538bc5353c6e7604
Laurynas Ruškys <bilbas@gmail.com>
2022-01-09 11:59:10 +0200

    Update

--- index.js
+++ index.js
@@ -1,11 +1,58 @@
-var i2c = require('i2c-bus');
-var MPU6050 = require('i2c-mpu6050');
- 
-var address = 0x68;
-var i2c1 = i2c.openSync(1);
- 
-var sensor = new MPU6050(i2c1, address);
- 
-var data = sensor.readSync();
-console.log(data);
+import { InterruptMonitor, MPU6050, Utils } from "@ros2jsguy/mpu6050-motion-data";
+
+const GPIO_MPU6050_DATA_PIN = 18;
+
+function main() {
+  let interrupts = 0;
+
+  const imu = new MPU6050();
+  imu.initialize();
+  
+  console.log('MPU6050 Device')
+  console.log('       connected: ', imu.testConnection());
+  console.log('              id: ', imu.getDeviceID());
+  console.log('  temperature(F): ', Utils.celciusToF(imu.getTemperature()).toFixed(2));
+  Utils.msleep(500);
+
+  console.log('\nDMP initialize and calibrate...');
+  imu.dmpInitialize();
+
+  // calibrate sensors
+  imu.calibrateAccel();
+  imu.calibrateGyro();
+  imu.printActiveOffsets();
+
+  // setup interrupt
+  imu.setInterruptLatchEnabled(true);
+  imu.setInterruptDMPEnabled(true);
+
+  // setup interrupt event monitoring and handling
+  const interruptMonitor = new InterruptMonitor(GPIO_MPU6050_DATA_PIN);
+  interruptMonitor.on('data', () => {
+      if (++interrupts === 1) console.log('  Receiving interrupt(s)');
+      const buf = imu.dmpGetCurrentFIFOPacket();
+      if (buf) {
+        const data = imu.dmpGetMotionData(buf);
+        console.log(data);
+      }
+  });
+  interruptMonitor.on('error', (error: Error) => {
+    console.log('Data error:', error.message);
+  });
+  interruptMonitor.start();
+
+  // run for 10 seconds then shutdown process
+  setTimeout(()=>{
+    imu.shutdown();
+    interruptMonitor.shutdown();
+    
+    process.exit(0);
+  }, 10000);
+
+  console.log('\nSampling data for 10 seconds');
+  imu.setDMPEnabled(true); // start DMP, data-ready interrupts should be raised
+  console.log('  Waiting for interrupts');
+}
+
+main();
 
